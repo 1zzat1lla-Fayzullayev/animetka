0aec450131f736c10d08c92dba949fd718ffe1ad
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-01-25 20:53:59 +0500

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./routes/home";
 import SinglePage from "./routes/singlePage";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<SinglePage />} />
-        </Routes>
-      </BrowserRouter>
+      {loading ? (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<SinglePage />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </>
   );
 }
