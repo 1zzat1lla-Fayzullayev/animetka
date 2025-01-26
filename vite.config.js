5a49688937e2a1f908408ae48de37c845085aaad
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-01-26 15:07:09 +0500

    change

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/anime_data": {
-        target: "https://animetka.ru", 
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+  build: {
+    outDir: 'dist', 
   },
 });
